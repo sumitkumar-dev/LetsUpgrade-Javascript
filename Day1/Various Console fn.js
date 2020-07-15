042691b5873d72acdc454324adc045349a817f92
sumitkumar-dev <66273281+sumitkumar-dev@users.noreply.github.com>
2020-07-15 18:09:52 +0530

    Add files via upload

---
+++ Day1/Various Console fn.js
@@ -0,0 +1,8 @@
+//alert  ('Lets Upgrade-Assignment Day 1')
+
+
+console.log ("Hey you cannot see me on the browser coz I am here")
+console.error("An error occoured")
+console.warn ("This is a warning")
+console.count (33.5)
+//console.clear ()
---
+++ Day1/varletconst.js
@@ -0,0 +1,18 @@
+// var example
+
+var friend = {
+    name : "Sumit",
+}
+console.log(friend);
+
+// let example
+
+{
+    let age = "24";
+console.log (age);
+}
+
+// const example
+
+const Nationality = "Indian";
+console.log (Nationality);
