89b1f10743b148eb3f5551e0ad6f2bcff8c92a05
sumitkumar-dev <66273281+sumitkumar-dev@users.noreply.github.com>
2020-07-16 13:57:00 +0530

    Add files via upload

---
+++ Day2/daytwo.js
@@ -0,0 +1,43 @@
+console.log ("Js linked to HTML")
+//Question 1
+let rollNo = prompt('What is your roll no.?');
+    console.log (rollNo)
+
+//Question 2
+
+//to measure length
+var text = "Harry had a little lamb";
+console.log(text.length)
+
+//find string within an string
+var text = "Harry had a little lamb";
+console.log (text.indexOf('lamb'))
+
+//slice string
+var text = "Harry had a little lamb";
+console.log (text.slice(10,14))
+
+//Replacing String Content
+var text = "Harry had a little lamb";
+console.log (text.replace("lamb","dog"))
+
+//Converting Arrays to Strings
+var animals = ["Dog", "Cat", "Cow"]
+console.log (animals.toString())
+
+//popping
+var animals = ["Dog", "Cat", "Cow"]
+console.log (animals.pop())
+
+
+//new array length
+var animals = ["Dog", "Cat", "Cow"]
+console.log (animals.push("Lion"))
+
+//Shift Element
+var animals = ["Dog", "Cat", "Cow"]
+console.log (animals.shift())
+
+
+
+    
